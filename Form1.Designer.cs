fb1509a5bbc0658688862f82ba95b1718a308c66
Kodama <jccfullstack@gmail.com>
2020-02-22 16:34:12 +0100

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Kodama_Xml_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_name_file = new System.Windows.Forms.Label();
+            this.textBox_drag = new System.Windows.Forms.TextBox();
+            this.button_translate = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox_next = new System.Windows.Forms.TextBox();
+            this.button_save_file = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_tag = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_ignore_txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_name_file
+            // 
+            this.lbl_name_file.AutoSize = true;
+            this.lbl_name_file.Location = new System.Drawing.Point(11, 9);
+            this.lbl_name_file.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_name_file.Name = "lbl_name_file";
+            this.lbl_name_file.Size = new System.Drawing.Size(77, 13);
+            this.lbl_name_file.TabIndex = 0;
+            this.lbl_name_file.Text = "Drag file in box";
+            // 
+            // textBox_drag
+            // 
+            this.textBox_drag.AllowDrop = true;
+            this.textBox_drag.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.textBox_drag.Location = new System.Drawing.Point(12, 25);
+            this.textBox_drag.Multiline = true;
+            this.textBox_drag.Name = "textBox_drag";
+            this.textBox_drag.Size = new System.Drawing.Size(428, 255);
+            this.textBox_drag.TabIndex = 1;
+            this.textBox_drag.TabStop = false;
+            this.textBox_drag.Text = "DragDrop File Here";
+            this.textBox_drag.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox_drag__DragDrop);
+            this.textBox_drag.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox_drag_DragEnter);
+            // 
+            // button_translate
+            // 
+            this.button_translate.Location = new System.Drawing.Point(365, 294);
+            this.button_translate.Name = "button_translate";
+            this.button_translate.Size = new System.Drawing.Size(75, 23);
+            this.button_translate.TabIndex = 2;
+            this.button_translate.Text = "Translate";
+            this.button_translate.UseVisualStyleBackColor = true;
+            this.button_translate.Click += new System.EventHandler(this.button_translate_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox_next
+            // 
+            this.textBox_next.Location = new System.Drawing.Point(446, 25);
+            this.textBox_next.Multiline = true;
+            this.textBox_next.Name = "textBox_next";
+            this.textBox_next.Size = new System.Drawing.Size(428, 255);
+            this.textBox_next.TabIndex = 4;
+            // 
+            // button_save_file
+            // 
+            this.button_save_file.Location = new System.Drawing.Point(799, 299);
+            this.button_save_file.Name = "button_save_file";
+            this.button_save_file.Size = new System.Drawing.Size(75, 23);
+            this.button_save_file.TabIndex = 5;
+            this.button_save_file.Text = "Save File";
+            this.button_save_file.UseVisualStyleBackColor = true;
+            this.button_save_file.Click += new System.EventHandler(this.button_save_file_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 299);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name Tag to Translate:";
+            // 
+            // textBox_tag
+            // 
+            this.textBox_tag.Location = new System.Drawing.Point(138, 296);
+            this.textBox_tag.Name = "textBox_tag";
+            this.textBox_tag.Size = new System.Drawing.Size(207, 20);
+            this.textBox_tag.TabIndex = 7;
+            this.textBox_tag.Text = "<value>";
+            this.textBox_tag.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 327);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Code to ignore";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox_ignore_txt
+            // 
+            this.textBox_ignore_txt.Location = new System.Drawing.Point(104, 327);
+            this.textBox_ignore_txt.Name = "textBox_ignore_txt";
+            this.textBox_ignore_txt.Size = new System.Drawing.Size(41, 20);
+            this.textBox_ignore_txt.TabIndex = 10;
+            this.textBox_ignore_txt.Text = "&jks;";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 359);
+            this.Controls.Add(this.textBox_ignore_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_tag);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_save_file);
+            this.Controls.Add(this.textBox_next);
+            this.Controls.Add(this.button_translate);
+            this.Controls.Add(this.textBox_drag);
+            this.Controls.Add(this.lbl_name_file);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_name_file;
+        private System.Windows.Forms.TextBox textBox_drag;
+        private System.Windows.Forms.Button button_translate;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox_next;
+        private System.Windows.Forms.Button button_save_file;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_tag;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_ignore_txt;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace Kodama_Xml_Control
+{
+    public partial class Form1 : Form
+    {
+        string line = string.Empty;
+        string[] files_dragdrop;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_drag_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void textBox_drag__DragDrop(object sender, DragEventArgs e)
+        {
+            files_dragdrop = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            lbl_name_file.Text = files_dragdrop[0];
+            TextBox dragdrop = textBox_drag;
+            ReadFileLine(files_dragdrop, dragdrop);
+        }
+        private void ReadFileLine(string[] files_dragdrop, TextBox dragdrop)
+        {
+            //leer el archivo
+            StreamReader file_read = File.OpenText(files_dragdrop[0]);
+            try
+            {
+                dragdrop.Text = string.Empty;
+                while ((line = file_read.ReadLine()) != null)
+                {
+                    dragdrop.Text += line + "\r\n";
+                }
+            }
+            catch { }
+        }
+
+        private void button_save_file_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (lbl_name_file.Text == "Drag file in box")
+            {
+                MessageBox.Show("DragDrop file", "Info");
+            }
+            else
+            {
+                saveFileDialog1.InitialDirectory = lbl_name_file.Text;
+                saveFileDialog1.Title = "Save text Files";
+                saveFileDialog1.Filter = "All files (*.*)|*.*";
+                saveFileDialog1.FilterIndex = 2;
+                saveFileDialog1.RestoreDirectory = true;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    //textBox1.Text = saveFileDialog1.FileName;
+                    //File.WriteAllText(saveFileDialog1.FileName, textBox_drag.Text);
+                    //IEnumerable<string> texttofile = textBox_drag.Text.;
+                    //File.AppendAllLines(saveFileDialog1.FileName, texttofile);
+                    using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                    {
+                        sw.WriteLine(textBox_next.Text);
+                    }
+                }
+            }
+        }
+
+        private void button_translate_Click(object sender, EventArgs e)
+        {
+            //textBox_next.Text = textBox_drag.Text;
+            //textBox_next.Text = TranslateText("","en","es");
+            try
+            {
+                TextBox dragdrop = textBox_next;
+                ReadFileLineToTranslate(files_dragdrop, dragdrop, "en", "es");
+            }
+            catch
+            {
+                MessageBox.Show("Select File", "Error");
+            }
+
+
+        }
+        private void ReadFileLineToTranslate(string[] files_dragdrop, TextBox dragdrop, string language_ori, string language_dest)
+        {
+            //control tag
+            string tag_start = textBox_tag.Text.Trim();
+            string tag_finish = ConverTagInitialtofinal(textBox_tag.Text, 1);
+            string symbol_1 = textBox_ignore_txt.Text.Trim();
+            //leer el archivo
+            StreamReader file_read = File.OpenText(files_dragdrop[0]);
+
+            try
+            {
+                while ((line = file_read.ReadLine()) != null)
+                {
+                    if (line.StartsWith(tag_start))
+                    {
+                        string textNotTag = line.Replace(tag_start, "");
+                        textNotTag = textNotTag.Replace(tag_finish, "");
+                        if (textNotTag.Contains(symbol_1))
+                        {
+                            textNotTag = textNotTag.Replace(symbol_1, "123456789123456789123456789");
+                            textNotTag = TranslateText(textNotTag, language_ori, language_dest);
+                            string final_txt = textNotTag.Replace("123456789123456789123456789", symbol_1);
+                            dragdrop.Text += tag_start + final_txt + tag_finish + "\r\n";
+                        }
+                        else
+                        {
+                            //print el text translated
+                            textNotTag = TranslateText(textNotTag, language_ori, language_dest);
+                            dragdrop.Text += tag_start + textNotTag + tag_finish + "\r\n";
+                        }
+
+                    }
+                    else
+                    {
+                        dragdrop.Text += line + "\r\n";
+                    }
+
+                }
+            }
+            catch { }
+        }
+
+        private string ConverTagInitialtofinal(string tag_final, int position)
+        {
+
+            string alpha = tag_final;
+            var builder = new StringBuilder();
+            int count = 0;
+            bool finish = false;
+
+            foreach (var c in alpha)
+            {
+                builder.Append(c);
+                if ((++count % position) == 0)
+                {
+                    if (!finish)
+                    {
+                        builder.Append('/');
+                        finish = true;
+                    }
+                }
+            }
+            Console.WriteLine("Before: {0}", alpha);
+            alpha = builder.ToString();
+            Console.WriteLine("After: {0}", alpha);
+            return alpha;
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public string TranslateText(string input, string orig_language, string dest_language)
+        {
+            // Set the language from/to in the url (or pass it into this function)
+            string url = String.Format
+            ("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
+             orig_language, dest_language, Uri.EscapeUriString(input));
+            HttpClient httpClient = new HttpClient();
+            string result = httpClient.GetStringAsync(url).Result;
+
+            // Get all json data
+            var jsonData = new JavaScriptSerializer().Deserialize<List<dynamic>>(result);
+
+            // Extract just the first array element (This is the only data we are interested in)
+            var translationItems = jsonData[0];
+
+            // Translation Data
+            string translation = "";
+
+            // Loop through the collection extracting the translated objects
+            foreach (object item in translationItems)
+            {
+                // Convert the item array to IEnumerable
+                IEnumerable translationLineObject = item as IEnumerable;
+
+                // Convert the IEnumerable translationLineObject to a IEnumerator
+                IEnumerator translationLineString = translationLineObject.GetEnumerator();
+
+                // Get first object in IEnumerator
+                translationLineString.MoveNext();
+
+                // Save its value (translated text)
+                translation += string.Format(" {0}", Convert.ToString(translationLineString.Current));
+            }
+
+            // Remove first blank character
+            if (translation.Length > 1) { translation = translation.Substring(1); };
+
+            // Return translation
+            return translation;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
